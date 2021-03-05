d56f22ba4c221441171f6a649cf814142c474d37
N.C Wu <mythnc@gmail.com>
2021-03-05 21:49:57 +0900

    Add utf8 insert version

--- lua/ex4_3.lua
+++ lua/ex4_3.lua
@@ -7,6 +7,27 @@ local function insert(dest_str, index, target_str)
   return prefix .. target_str .. string.sub(dest_str, index, -1)
 end
 
+
+
+local function insert_utf8(dest_str, char_index, target_str)
+  local result = ""
+  local is_insert = false
+  for i, c in utf8.codes(dest_str) do
+    if i == utf8.offset(dest_str, char_index) then
+      result = result .. target_str
+      is_insert = true
+    end
+    result = result .. utf8.char(c)
+  end
+
+  if not is_insert then
+    result = result .. target_str
+  end
+
+  return result
+end
+
+
 print(insert("hello world", 7, "small ")) --> hello small world
 print(insert("hello world", 11, "small ")) --> hello worldsmall
 print(insert("hello world", 12, "small ")) --> hello worldsmall
@@ -17,3 +38,12 @@ local target_s = "d"
 for i = 1, #s+1 do
   print(insert(s, i, target_s))
 end
+
+print(string.rep("-", 10))
+
+print("ação")
+print(insert_utf8("ação", 1, "!"))
+print(insert_utf8("ação", 2, "!"))
+print(insert_utf8("ação", 3, "!"))
+print(insert_utf8("ação", 4, "!"))
+print(insert_utf8("ação", 5, "!"))
