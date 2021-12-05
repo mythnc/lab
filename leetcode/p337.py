9f69482a063e3cfce2ce8832c2ef1425658c31b9
N.C Wu <mythnc@gmail.com>
2021-12-05 16:21:05 +0900

    Update leetcode 337

--- leetcode/p337.py
+++ leetcode/p337.py
@@ -14,25 +14,14 @@ class Solution:
         if root is None:
             return 0
 
-        # cond1: no self + children
-        cond1_sum = 0
+        sum_ = root.val
         if (left_node := root.left) is not None:
-            self.map_[left_node] = self.rob(left_node)
-            cond1_sum += self.map_[left_node]
+            sum_ += self.rob(left_node.left) + self.rob(left_node.right)
+
         if (right_node := root.right) is not None:
-            self.map_[right_node] = self.rob(right_node)
-            cond1_sum += self.map_[right_node]
-
+            sum_ += self.rob(right_node.left) + self.rob(right_node.right)
+    
+        # cond1: no self + children
         # cond2: self + grandchildren
-        cond2_sum = root.val
-        if left_node is not None:
-            self.map_[left_node.left] = self.rob(left_node.left)
-            self.map_[left_node.right] = self.rob(left_node.right)
-            cond2_sum += self.map_[left_node.left] + self.map_[left_node.right]
-
-        if right_node is not None:
-            self.map_[right_node.left] = self.rob(right_node.left)
-            self.map_[right_node.right] = self.rob(right_node.right)
-            cond2_sum += self.map_[right_node.left] + self.map_[right_node.right]
-
-        return max(cond1_sum, cond2_sum)
+        self.map_[root] = max(self.rob(root.left) + self.rob(root.right) , sum_)
+        return self.map_[root]
