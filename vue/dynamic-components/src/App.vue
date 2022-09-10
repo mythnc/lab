85ec8a414e851f059bd36c8bfd164dad7d1899c3
N.C Wu <mythnc@gmail.com>
2022-09-10 23:07:34 +0900

    Add dynamic component practice

---
+++ vue/dynamic-components/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <select name="" id="" v-model="currentTab">
+    <option value="Home">Home</option>
+    <option value="About">About</option>
+  </select>
+  <KeepAlive>
+    <component :is="currentTab"></component>
+  </KeepAlive>
+</template>
+
+<script>
+import Home from "@/components/Home.vue";
+import About from "@/components/About.vue";
+
+export default {
+  name: "App",
+  components: {
+    Home,
+    About,
+  },
+  data() {
+    return {
+      currentTab: "Home",
+    };
+  },
+};
+</script>
