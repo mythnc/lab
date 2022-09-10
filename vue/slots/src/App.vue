7779ac9e8d680e5ffa4d3e4abc4bbe1406ccf5ef
N.C Wu <mythnc@gmail.com>
2022-09-10 22:37:07 +0900

    Vue slot practice

---
+++ vue/slots/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <app-form>
+    <template #helper>{{ msg }}</template>
+    <template #fields>
+      <input type="text" placeholder="email" />
+    </template>
+    <template #buttons>
+      <button type="submit">Submit</button>
+    </template>
+  </app-form>
+  <app-form>
+    <template #helper>Form 2</template>
+    <template #fields>
+      <input type="text" placeholder="username" />
+    </template>
+    <template #buttons>
+      <button type="cancel">Cancel</button>
+    </template>
+  </app-form>
+</template>
+
+<script>
+import AppForm from "@/components/Form.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppForm,
+  },
+  data() {
+    return {
+      msg: "Form 1",
+    };
+  },
+};
+</script>
---
+++ vue/slots/src/components/Form.vue
@@ -0,0 +1,13 @@
+<template>
+  <form>
+    <div>
+      <slot name="helper"></slot>
+    </div>
+    <div>
+      <slot name="fields"></slot>
+    </div>
+    <div>
+      <slot name="buttons"></slot>
+    </div>
+  </form>
+</template>
