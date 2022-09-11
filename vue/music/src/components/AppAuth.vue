996bba0dda677a012de5b1eef76c87cf2685fa2f
N.C Wu <mythnc@gmail.com>
2022-09-11 20:42:53 +0900

    Closing the Modal

--- vue/music/src/components/AppAuth.vue
+++ vue/music/src/components/AppAuth.vue
@@ -18,7 +18,7 @@
           <div class="flex justify-between items-center pb-4">
             <p class="text-2xl font-bold">Your Account</p>
             <!-- Modal Close Button -->
-            <div class="modal-close cursor-pointer z-50">
+            <div class="modal-close cursor-pointer z-50" @click="closeModal">
               <i class="fas fa-times"></i>
             </div>
           </div>
@@ -136,13 +136,19 @@
 </template>
 
 <script>
-import { mapState } from "pinia";
+import { mapState, mapWritableState } from "pinia";
 import useModalStore from "@/stores/modal";
 
 export default {
   name: "AppAuth",
   computed: {
     ...mapState(useModalStore, ["hiddenClass"]),
+    ...mapWritableState(useModalStore, ["isOpen"]),
+  },
+  methods: {
+    closeModal() {
+      this.isOpen = false;
+    },
   },
 };
 </script>
