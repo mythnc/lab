a15ec1f0851277b631910d5a03459dfa39706fdd
N.C Wu <mythnc@gmail.com>
2022-09-19 22:42:20 +0900

    Custom Error Messages

--- vue/music/src/components/AppAuth.vue
+++ vue/music/src/components/AppAuth.vue
@@ -183,9 +183,9 @@ export default {
         email: "required|min:3|max:100|email",
         age: "required|min_value:18|max_value:100",
         password: "required|min:9|max:100|excluded:password",
-        confirm_password: "confirmed:@password",
-        country: "required|excluded:Unknown",
-        tos: "required",
+        confirm_password: "passwords_mismatch:@password",
+        country: "required|country_excluded:Unknown",
+        tos: "tos",
       },
       userData: {
         country: "USA",
