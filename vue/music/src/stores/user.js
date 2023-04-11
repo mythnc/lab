c91350c281aff712a22fbbf287e429678b3badcf
N.C Wu <mythnc@gmail.com>
2023-04-12 02:16:14 +0900

    Setting up the Login

--- vue/music/src/stores/user.js
+++ vue/music/src/stores/user.js
@@ -26,5 +26,10 @@ export default defineStore("user", {
 
       this.userLoggedIn = true;
     },
+    async authenticate(values) {
+      await auth.signInWithEmailAndPassword(values.email, values.password);
+
+      this.userLoggedIn = true;
+    },
   },
 });
