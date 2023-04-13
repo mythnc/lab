e0c3c539f16a78cb9f9f81907d7cfe3dc2dcc92f
N.C Wu <mythnc@gmail.com>
2023-04-14 00:53:44 +0900

    Creating Routes

--- vue/music/src/router/index.js
+++ vue/music/src/router/index.js
@@ -1,6 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-const routes = [];
+import About from "@/views/AboutView.vue";
+import Home from "@/views/HomeView.vue";
+
+const routes = [
+  {
+    path: "/",
+    component: Home,
+  },
+  {
+    path: "/about",
+    component: About,
+  },
+];
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
